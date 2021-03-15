628836d288df780c51f1ee3025106a0ea8a96a23
Vini Aswal <vijendersingh.aswal@sjsu.edu>
2021-03-14 19:43:33 -0700

    Added JSON to RDF for employee

--- ama-custom-kg/src/main/java/JSONReader.java
+++ ama-custom-kg/src/main/java/JSONReader.java
@@ -1,2 +1,44 @@
+import java.io.FileReader;
+
+import org.apache.jena.rdf.model.Resource;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class JSONReader {
+    public static void parseEmployeeObject(JSONObject employee) {
+        Resource danHarkey
+                = Test.MODEL.createResource(Test.AMA_BASE + "/" + employee.get("sjsuId"));
+
+        //System.out.println(employee.keySet());
+
+        for (Object key: employee.keySet()) {
+            System.out.println("key: " + key + " val: " + employee.get(key));
+            danHarkey.addProperty(Test.personelMap.get(key), employee.get(key).toString());
+        }
+
+        Test.printRDFObject();
+    }
+
+    public static void readJSONFile() {
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader(JSONReader.class.getResource("sjsu-entities.json").getFile()))
+        {
+            //Read JSON file
+            JSONObject obj = (JSONObject) jsonParser.parse(reader);
+
+            JSONArray employeeList = (JSONArray) obj.get("personel");
+            //System.out.println(employeeList);
+
+            //Iterate over employee array
+            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String []args) {
+        readJSONFile();
+    }
 }
--- ama-custom-kg/src/main/java/Test.java
+++ ama-custom-kg/src/main/java/Test.java
@@ -1,10 +1,13 @@
 import org.apache.jena.rdf.model.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
-    private static String AMA_BASE = "http://www.w3.org/2001/ama/sjsu";
+    public static String AMA_BASE = "http://www.w3.org/2001/ama/sjsu";
 
     // create an empty Model
-    private static Model MODEL = ModelFactory
+    public static Model MODEL = ModelFactory
             .createDefaultModel();
             //.setNsPrefix("", AMA_BASE);
 
@@ -12,22 +15,67 @@ public class Test {
     private static Property LAST_NAME_PROP = MODEL.createProperty(AMA_BASE + "#lastName");
     private static Property FULL_NAME_PROP = MODEL.createProperty(AMA_BASE + "#fullName");
     private static Property NAME_PROP = MODEL.createProperty(AMA_BASE + "#name");
+    private static Property TYPE_PROP = MODEL.createProperty(AMA_BASE + "#type");
+    private static Property DEPARTMENT_NAME_PROP = MODEL.createProperty(AMA_BASE + "#department");
+    private static Property SJSU_ID_PROP = MODEL.createProperty(AMA_BASE + "#sjsuId");
+
+    public static void printRDFObject() {
+        StmtIterator iter = MODEL.listStatements();
+
+        // print out the predicate, subject and object of each statement
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();  // get next statement
+            Resource  subject   = stmt.getSubject();     // get the subject
+            Property  predicate = stmt.getPredicate();   // get the predicate
+            RDFNode   object    = stmt.getObject();      // get the object
+
+            System.out.print("<" + subject.toString() + ">");
+            System.out.print(" <" + predicate.toString() + ">");
+            if (object instanceof Resource) {
+                System.out.print("<" + object.toString() + ">");
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+
+            System.out.println(" .");
+        }
+    }
+
+    public static  Map<String, Property> personelMap  = new HashMap<String, Property>() {{
+        put("firstName", FIRST_NAME_PROP);
+        put("lastName", LAST_NAME_PROP);
+        put("fullName", FULL_NAME_PROP);
+        put("type", TYPE_PROP);
+        put("department", DEPARTMENT_NAME_PROP);
+        put("sjsuId", SJSU_ID_PROP);
+    }};
 
     public static void main(String[] args) {
-        String personURI    = "JohnSmith";
-        String givenName    = "John";
-        String familyName   = "Smith";
+        String personURI    = "DanHarkey_12345";
+        String givenName    = "Dan";
+        String familyName   = "Harkey";
         String fullName     = givenName + " " + familyName;
+
 
         // create the resource
         //   and add the properties cascading style
-        Resource johnSmith
+        Resource danHarkey
                 = MODEL.createResource(AMA_BASE + "/" + personURI)
                 .addProperty(FULL_NAME_PROP, fullName)
-                .addProperty(NAME_PROP,
-                        MODEL.createResource()
-                                .addProperty(FIRST_NAME_PROP, givenName)
-                                .addProperty(LAST_NAME_PROP, familyName));
+                .addProperty(FIRST_NAME_PROP, givenName)
+                .addProperty(LAST_NAME_PROP, familyName);
+
+        Resource vg
+                = MODEL.createResource(AMA_BASE + "/" + "VinodhGopinath_789")
+                .addProperty(FULL_NAME_PROP, "Vinodh Gopinath")
+                .addProperty(FIRST_NAME_PROP, "Vinodh")
+                .addProperty(LAST_NAME_PROP, "Gopinath");
+
+//                .addProperty(NAME_PROP,
+//                        MODEL.createResource()
+//                                .addProperty(FIRST_NAME_PROP, givenName)
+//                                .addProperty(LAST_NAME_PROP, familyName));
 
         // list the statements in the Model
         StmtIterator iter = MODEL.listStatements();
@@ -39,10 +87,10 @@ public class Test {
             Property  predicate = stmt.getPredicate();   // get the predicate
             RDFNode   object    = stmt.getObject();      // get the object
 
-            System.out.print(subject.toString());
-            System.out.print(" " + predicate.toString() + " ");
+            System.out.print("<" + subject.toString() + ">");
+            System.out.print(" <" + predicate.toString() + ">");
             if (object instanceof Resource) {
-                System.out.print(object.toString());
+                System.out.print("<" + object.toString() + ">");
             } else {
                 // object is a literal
                 System.out.print(" \"" + object.toString() + "\"");
@@ -50,5 +98,20 @@ public class Test {
 
             System.out.println(" .");
         }
+
+//        StmtIterator stmtiter = MODEL.listStatements(
+//                new SimpleSelector(null, FULL_NAME_PROP, (RDFNode) null) {
+//                    public boolean selects(Statement s)
+//                    {return s.getString().endsWith("Smith");}
+//                });
+//
+//        if (stmtiter.hasNext()) {
+//            System.out.println("The database contains vcards for:");
+//            while (stmtiter.hasNext()) {
+//                System.out.println("  " + stmtiter.nextStatement().getObject());
+//            }
+//        } else {
+//            System.out.println("No vcards were found in the database");
+//        }
     }
 }
