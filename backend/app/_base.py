30c1b2baec9327c40d35dffc1fc2532b0ef32bef
Shivangi Jain <jshivangi15@gmail.com>
2021-03-30 15:21:14 -0700

    Backend folder structure and imports fixed

--- backend/app/_base.py
+++ backend/app/_base.py
@@ -3,13 +3,25 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
+import sys
+
+#sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+#import pdb;
+#pdb.set_trace()
+#sys.path.append('/Users/jainsh/Documents/cmpe295/NLP-based-QA-System-for-custom-KG-demo')
+import sys
+from os import path
+sys.path.append(path.dirname(__file__))
+#print(sys.path)
+#print(sys.path[1])
 from backend.app.dbpedia.main import quepy_main
 
-
 # creating a Flask app
+#import pdb; pdb.set_trace()
 app = Flask(__name__)
 CORS(app)  # Let the api acces for frontends.
 load_dotenv()
+
 
 
 # on the terminal type: flask run or curl http://127.0.0.1:5000/
@@ -31,7 +43,9 @@ def process():
         data = request.json
         question = data['question']
         print(question)
+        #str_return = 'answer'
         str_return = quepy_main(question)
+        # quepy_main(question)
         return str_return
 
     except Exception as e:
@@ -41,7 +55,7 @@ def process():
 
 def run_app():
     app.run(debug=True)
-    app.run(host='0.0.0.0', port = 5001)
+    app.run(host='0.0.0.0', port=5001)
 
 
 # driver function
--- backend/app/dbpedia/__init__.py
+++ backend/app/dbpedia/__init__.py
@@ -1,2 +1,1 @@
 
-# from backend.app.dbpedia.main import quepy_main
