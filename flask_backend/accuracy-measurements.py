9f4468bfa1f79bd4075d6c48989ce851d7c2c583
Vini Aswal <vijendersingh.aswal@sjsu.edu>
2021-04-15 10:17:39 +0530

    Various improvements

--- flask_backend/accuracy-measurements.py
+++ flask_backend/accuracy-measurements.py
@@ -50,7 +50,9 @@ if __name__ == '__main__':
     total = len(questions)
 
     for version in [1, 2, 3]:
-        correct = 0
+        number_of_correct_answers = 0
+        number_of_questions_answered = 0
+
         for q in questions:
             question = json.loads(json.dumps(q, indent=4))
             question.update({"version": version})
@@ -59,8 +61,24 @@ if __name__ == '__main__':
             print("question", question)
             print("answer", answer)
 
+            if len(answer) != 2:
+                number_of_questions_answered = number_of_questions_answered + 1
+
             if answer == question["answer"]:
-                correct = correct + 1
+                number_of_correct_answers = number_of_correct_answers + 1
+
+        if number_of_questions_answered == 0:
+             precision = 0
+        else:
+            precision = number_of_correct_answers/number_of_questions_answered
+        recall = number_of_correct_answers/total
+        if (precision + recall) == 0:
+            f_measure = 0
+        else:
+            f_measure = 2 * (precision * recall)/(precision + recall)
 
         print("version", version)
-        print("accuracy", correct/total)
+        print("precision", precision)
+        print("recall", recall)
+        print("f_measure", f_measure)
+
--- flask_backend/app.py
+++ flask_backend/app.py
@@ -7,6 +7,8 @@ import json
 import pkg_resources
 from symspellpy import SymSpell, Verbosity
 from difflib import get_close_matches
+from spacy.matcher import PhraseMatcher
+import re
 
 sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
 dictionary_path = pkg_resources.resource_filename(
@@ -15,7 +17,8 @@ sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
 nlp = spacy.load("en_core_web_trf")
 
-
+phrase_matcher = PhraseMatcher(nlp.vocab)
+prop_phrase_matcher = PhraseMatcher(nlp.vocab)
 
 app = Flask(__name__)
 CORS(app)  # Let the api access for frontends.
@@ -33,7 +36,7 @@ def get_entity_names(url):
     url = 'http://localhost:3030/ama/sparql?query=%s' % (percent_encoded_sparql)
     response = requests.post(url)
     print(response.json())
-    #return json.dumps(response.json())
+    # return json.dumps(response.json())
 
     if "results" in response.json():
         bindings = response.json()["results"]["bindings"]
@@ -42,17 +45,57 @@ def get_entity_names(url):
         return []
 
 
-entity_names = set(get_entity_names("<http://www.w3.org/2001/ama/sjsu#name>"))
-#entity_properties = set(get_entity_names("<http://www.w3.org/2001/ama/sjsu#name>"))
+def get_entity_properties():
+    query = """
+                SELECT DISTINCT ?predicate 
+                WHERE {
+                  ?entity ?predicate ?object .
+                }
+            """
+    percent_encoded_sparql = quote(query, safe='')
+
+    url = 'http://localhost:3030/ama/sparql?query=%s' % (percent_encoded_sparql)
+    response = requests.post(url)
+    print(response.json())
+    # return json.dumps(response.json())
+
+    if "results" in response.json():
+        bindings = response.json()["results"]["bindings"]
+        ans = []
+        for binding in bindings:
+            match = re.search(r'http://www.w3.org/2001/ama/sjsu#?([^\']+)', binding["predicate"]["value"])
+            # ans.append(binding["predicate"]["value"].split("#")[1])
+            if match:
+                print(match.group(1))
+                ans.append(match.group(1))
+
+        print(ans)
+        return ans
+        # return [binding["res"]["value"] for binding in bindings]
+    else:
+        return []
+
+
+entity_names_arr = get_entity_names("<http://www.w3.org/2001/ama/sjsu#name>")
+entity_names = set(entity_names_arr)
+# phrases = ['machine learning', 'robots', 'intelligent agents']
+phrases = entity_names_arr
+patterns = [nlp(text) for text in phrases]
+phrase_matcher.add('AI', None, *patterns)
+entity_properties = get_entity_properties()
+prop_patterns = [nlp(text) for text in entity_properties]
+prop_phrase_matcher.add('property', None, *prop_patterns)
+
 
 # on the terminal type: flask run or curl http://127.0.0.1:5000/
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
 @app.route('/', methods=['GET'])
 def home():
-    if(request.method == 'GET'):
+    if (request.method == 'GET'):
         data = "hello world"
         return jsonify({'data': data})
+
 
 # post request accepts a query argument value
 # return status string
@@ -70,6 +113,17 @@ def process():
 
         langCode = "en"
         doc = nlp(question)
+        entity_set = []
+        property_set = []
+
+        matched_phrases = phrase_matcher(doc)
+        for match_id, start, end in matched_phrases:
+            string_id = nlp.vocab.strings[match_id]
+            span = doc[start:end]
+            print(match_id, string_id, start, end, span.text)
+            entity_set.append(span.text)
+
+
 
         if version >= 2:
             print("version", version)
@@ -95,8 +149,14 @@ def process():
 
                 doc = nlp(" ".join(tokens))
 
+        matched_phrases_prop = prop_phrase_matcher(doc)
+        for match_id, start, end in matched_phrases_prop:
+            string_id = nlp.vocab.strings[match_id]
+            span = doc[start:end]
+            print(match_id, string_id, start, end, span.text)
+            property_set.append(span.text)
+
         entities = doc.ents
-        entitySet = []
 
         print("** entities")
         for entity in entities:
@@ -107,19 +167,21 @@ def process():
                 if entity.text not in entity_names:
                     close_matches = get_close_matches(entity.text, entity_names)
                     for close_match in close_matches:
-                        entitySet.append(close_match)
-                        break
+                        if close_match not in entity_set:
+                            entity_set.append(close_match)
+                            break
             else:
-                entitySet.append(str(entity))
+                if str(entity) not in entity_set:
+                    entity_set.append(str(entity))
 
         print("entitySet")
-        print(entitySet)
+        print(entity_set)
 
         for token in doc:
             print(token, token.pos_)
 
-        print("Nouns:", [token.text for token in doc if token.pos_ == "NOUN"])
-        pos_nouns = [token.text for token in doc if token.pos_ == "NOUN" and len(token) >= 2]
+        # print("Nouns:", [token.text for token in doc if token.pos_ == "NOUN"])
+        # pos_nouns = [token.text for token in doc if token.pos_ == "NOUN" and len(token) >= 2]
 
         print("chunks")
         chunks = set()
@@ -128,18 +190,18 @@ def process():
 
         # Try to find entities from chunks
         if version >= 3:
-            if len(entitySet) == 0:
+            if len(entity_set) == 0:
                 for chunk in doc.noun_chunks:
                     if chunk.label_ == "NP":
                         close_matches = get_close_matches(chunk.text, entity_names)
                         print("entity.text", chunk.text)
                         print("close_matches", close_matches)
                         for close_match in close_matches:
-                            entitySet.append(close_match)
+                            entity_set.append(close_match)
                             break
 
-        if (len(entitySet) == 1) and (len(pos_nouns) == 1):
-            result_obj = one_entity_one_predicate(entitySet, pos_nouns, langCode)
+        if (len(entity_set) == 1) and (len(property_set) == 1):
+            result_obj = one_entity_one_predicate(entity_set, property_set, langCode)
             print("result_obj", result_obj)
             result = json.loads(result_obj)
 
@@ -149,33 +211,33 @@ def process():
                     return binding["answer"]["value"]
                     break
 
-            return ""
-        elif len(entitySet) == 1 and len(pos_nouns) != 1:
+            return getQueryResults(entity_set, langCode)
+        elif len(entity_set) == 1 and len(property_set) != 1:
             # print("pos_nouns", pos_nouns)
             # result = json.loads(result_obj)
             # answer = result["results"]["bindings"][0]["answer"]["value"]
             # print(answer)
             # return(answer)
-            return getQueryResults(entitySet, langCode)
+            return getQueryResults(entity_set, langCode)
         else:
-            return getQueryResults(entitySet, langCode)
+            return getQueryResults(entity_set, langCode)
 
     except Exception as e:
         print(e)
         return "Error occurred!!" + e
 
 
-def one_entity_one_predicate(entitySet, pos_nouns, langCode):
+def one_entity_one_predicate(entitySet, property_set, langCode):
     data = {'entities': []}
     print("entity", entitySet[0])
     entity = entitySet[0]
-    print("noun", pos_nouns[0])
-    noun = pos_nouns[0]
+    print("property", property_set[0])
+    noun = property_set[0]
 
     query = """
         SELECT ?answer 
         WHERE {
-          
+
           ?subject <http://www.w3.org/2001/ama/sjsu#name> "%s" .
           ?subject <http://www.w3.org/2001/ama/sjsu#%s> ?answer
         }
@@ -188,13 +250,13 @@ def one_entity_one_predicate(entitySet, pos_nouns, langCode):
     print(response.json())
     return json.dumps(response.json())
 
+
 def getQueryResults(entitySet, langCode):
-        data = {}
-        return json.dumps(data)
+    data = {}
+    return json.dumps(data)
 
 
 # driver function
 if __name__ == '__main__':
-
     app.run(debug=False)
-    #app.run(host='0.0.0.0', port = 5001)
+    # app.run(host='0.0.0.0', port = 5001)
