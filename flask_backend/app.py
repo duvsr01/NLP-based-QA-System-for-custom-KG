dce7060fc9cdf786ccfa0432e4377a5666b27c06
sakshimahendrusjsu <sakshi.mahendru@sjsu.edu>
2021-04-25 04:19:54 -0700

    rest default port setting for flask backend

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -400,5 +400,5 @@ def getQueryResults(entitySet, langCode):
 
 # driver function
 if __name__ == '__main__':
-    # app.run(debug=False)
-    app.run(host='0.0.0.0', port = 5001)
+    app.run(debug=False)
+    # app.run(host='0.0.0.0', port = 5000)
